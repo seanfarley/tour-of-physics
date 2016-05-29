8a27f9e741e1acdf64672cb8a068c5aca8be1f9b
Sean Farley <sean@farley.io>
2016-05-28 19:43:26 -0700

    title: remove 'documentation' since this is a book

--- conf.py
+++ conf.py
@@ -229,7 +229,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'TourofPhysics.tex', u'Tour of Physics Documentation',
+    (master_doc, 'TourofPhysics.tex', u'Tour of Physics',
      u'Robert Dalling', 'manual'),
 ]
 
@@ -259,7 +259,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'tourofphysics', u'Tour of Physics Documentation',
+    (master_doc, 'tourofphysics', u'Tour of Physics',
      [author], 1)
 ]
 
@@ -273,8 +273,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'TourofPhysics', u'Tour of Physics Documentation',
-     author, 'TourofPhysics', 'One line description of project.',
+    (master_doc, 'TourofPhysics', u'Tour of Physics',
+     author, 'TourofPhysics', 'Open-source Physics Textbook.',
      'Miscellaneous'),
 ]
 
--- index.rst
+++ index.rst
@@ -1,10 +1,8 @@
-.. Tour of Physics documentation master file, created by
+.. Tour of Physics master file, created by
    sphinx-quickstart on Sat May 28 18:59:18 2016.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
 
-Welcome to Tour of Physics's documentation!
-===========================================
+Tour of Physics
+===============
 
 Contents:
 
